813e62919c24c853026bbd4b09ef42b72ec57d0b
Daniel Bretoi <daniel@otherware.org>
2025-06-11 17:09:46 +0700

    add minimal readme

--- functions/reef_show_help.fish
+++ functions/reef_show_help.fish
@@ -2,9 +2,9 @@ function reef_show_help --description 'show help for subcommands based on comple
     set -l cmd $argv
     echo "Usage: $cmd <subcommand> [options]"
     echo "Subcommands:"
-    set -l regex 'complete .* -d \'([^\']+)\'.*-a \'?([^ ]+).*'
+    set -l regex 'complete .* -d \'([^\']+)\'.*-a \'?([a-z ]+).*'
     for line in (complete -c $cmd | fish_grep "$cmd -d .*-a \'?[a-z]+")
-        set -l subcmd (string replace -r $regex '$2' $line)
+        set -l subcmd (string replace ' ' '/' ( string trim (string replace -r $regex '$2' $line)))
         set -l description (string replace -r $regex '$1' $line)
         printf "  %-13s %s\n" "$subcmd" "$description"
     end | sort
---
+++ readme.md
@@ -0,0 +1,12 @@
+ _ __ ___  ___ / _|
+| '__/ _ \/ _ \ |_
+| | |  __/  __/  _|
+|_|  \___|\___|_|  🪸🐟
+
+Usage: reef <subcommand> [options]
+Subcommands:
+  add/install   add corals
+  ls/list       list installed corals
+  rm/remove     remove corals
+  up/update     update coral(s)
+
