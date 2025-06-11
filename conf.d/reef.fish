997ac3b74da9a8b51629aa4ed9de3991eb4c79b5
Daniel Bretoi <daniel@otherware.org>
2025-06-11 10:52:47 +0700

    fixes to not accidentally source things

--- conf.d/reef.fish
+++ conf.d/reef.fish
@@ -1,9 +1,9 @@
 # Add all coral functions/ directories to fish_function_path
-set -l coral_paths (path resolve $__fish_config_dir/corals/**/functions)
+set -l coral_paths (path resolve $__fish_config_dir/corals/*/*/functions)
 set -a fish_function_path $coral_paths
 
 # Source all coral conf.d/*.fish files
-for conf in (path resolve $__fish_config_dir/corals/**/conf.d/*.fish)
+for conf in (path resolve $__fish_config_dir/corals/*/*/conf.d/*.fish)
     set base (basename $conf)
     if [ $base != "reef.fish" ]
         source $conf
@@ -12,6 +12,6 @@ for conf in (path resolve $__fish_config_dir/corals/**/conf.d/*.fish)
 end
 #
 # Source all coral complete/*.fish files
-for complete in (path resolve $__fish_config_dir/corals/**/completions/*.fish)
+for complete in (path resolve $__fish_config_dir/corals/*/*/completions/*.fish)
     source $complete
 end
