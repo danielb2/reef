d7f17620acf5a82c101ba97277c4486926f4787a
Daniel Bretoi <daniel@otherware.org>
2025-06-17 17:55:23 +0700

    fix sourcing

--- conf.d/reef.fish
+++ conf.d/reef.fish
@@ -1,6 +1,5 @@
-# Add all coral functions/ directories to fish_function_path
-set -l coral_paths (path resolve $__fish_config_dir/corals/*/*/functions)
-set -a fish_function_path $coral_paths
+set -a fish_function_path (path resolve $__fish_config_dir/corals/*/*/functions)
+set -a fish_complete_path (path resolve $__fish_config_dir/corals/*/*/completions)
 
 # Source all coral conf.d/*.fish files
 for conf in (path resolve $__fish_config_dir/corals/*/*/conf.d/*.fish)
@@ -8,9 +7,4 @@ for conf in (path resolve $__fish_config_dir/corals/*/*/conf.d/*.fish)
     if [ $base != "reef.fish" ]
         source $conf
     end
-end
-
-# Source all coral complete/*.fish files
-for complete in (path resolve $__fish_config_dir/corals/*/*/completions/*.fish)
-    source $complete
 end
