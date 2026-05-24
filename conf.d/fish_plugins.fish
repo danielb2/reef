d501f2d26318e4e451433280a42de57f356060b1
Daniel Bretoi <daniel@otherware.org>
2026-05-24 18:09:38 +0700

    only use fish_plugins file if it exist. better naming
    1. it shuold be back to fish_plugins_file
    2. if the file doesn't exist, don't use it
    3. if it exists and is empty. populate it with the results from reef ls before doing the reef add

--- conf.d/fish_plugins.fish
+++ conf.d/fish_plugins.fish
@@ -1,21 +1,24 @@
 function __reef_fish_plugins_add --on-event reef_add
     set -l repo $argv[1]
-    set -l corals_file $__fish_config_dir/fish_plugins
+    set -l fish_plugins_file $__fish_config_dir/fish_plugins
+    test -f $fish_plugins_file; or return
+
+    # If the file exists but is empty, bootstrap it first
+    if not string match -rq '[^[:space:]]' < $fish_plugins_file
+        reef ls > $fish_plugins_file
+    end
+
     set -l portable_repo (string replace "$HOME" "~" -- "$repo")
-    if test -f $corals_file
-        set -l plugins (cat $corals_file)
-        if not contains "$portable_repo" $plugins
-            echo "$portable_repo" >> $corals_file
-        end
-    else
-        echo "$portable_repo" >> $corals_file
+    set -l plugins (cat $fish_plugins_file)
+    if not contains "$portable_repo" $plugins
+        echo "$portable_repo" >> $fish_plugins_file
     end
 end
 
 function __reef_fish_plugins_rm --on-event reef_rm
     set -l coral $argv[1]
-    set -l corals_file $__fish_config_dir/fish_plugins
-    test -f $corals_file; or return
+    set -l fish_plugins_file $__fish_config_dir/fish_plugins
+    test -f $fish_plugins_file; or return
     set -l tmp (mktemp)
     set -l coral_slug (__reef_resolve_coral $coral)
     while read -l line
@@ -23,16 +26,22 @@ function __reef_fish_plugins_rm --on-event reef_rm
         if test "$resolved" != "$coral_slug"
             echo "$line" >> $tmp
         end
-    end < $corals_file
-    mv $tmp $corals_file
+    end < $fish_plugins_file
+    mv $tmp $fish_plugins_file
 end
 
 function __reef_fish_plugins_update --on-event reef_update
-    set -l corals_file $__fish_config_dir/fish_plugins
-    test -f $corals_file; or return
+    set -l fish_plugins_file $__fish_config_dir/fish_plugins
+    test -f $fish_plugins_file; or return
 
-    set -l desired_lines (string trim < $corals_file | string match -r -v '^#|^$')
+    set -l desired_lines (string trim < $fish_plugins_file | string match -r -v '^#|^$')
     set -l installed (__reef_list)
+
+    # If the file exists but is empty, populate it from current disk state
+    if not set -q desired_lines[1]
+        reef ls > $fish_plugins_file
+        return
+    end
 
     set -l missing
     set -l desired_corals
