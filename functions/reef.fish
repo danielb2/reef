b959b8cf2e1e7dd1eb04e1cf77c7c395c081c2f4
Daniel Bretoi <daniel@otherware.org>
2026-05-24 12:22:37 +0700

    latest version with emits etc

--- functions/reef.fish
+++ functions/reef.fish
@@ -12,7 +12,6 @@ function __reef_list
 end
 
 function __reef_add
-    set -l corals_file $__fish_config_dir/fish_plugins
     if not command -sq git
         echo 🪸🐟 (set_color red)yeah, you need to install `git` first(set_color normal)
         return 1
@@ -23,7 +22,7 @@ function __reef_add
         # If it looks like a local path but doesn't exist, skip it early
         if string match -rq '^[~/]' -- "$repo"
             if not test -d "$expanded_repo"
-                echo 🪸🐟 (set_color yellow)warning: local path not found: $repo(set_color normal)
+                echo "🪸🐟 (set_color yellow)warning: local path not found: $repo(set_color normal)"
                 continue
             end
         end
@@ -60,17 +59,8 @@ function __reef_add
 
         git clone $clone_args -- "$base" "$path"
         if test $status -eq 0
+            emit reef_add "$repo"
             emit {$name}_install
-            # Save the portable version (~ instead of /Users/...) to config
-            set -l portable_repo (string replace "$HOME" "~" -- "$repo")
-            if test -f $corals_file
-                set -l plugins (cat $corals_file)
-                if not contains "$portable_repo" $plugins
-                    echo "$portable_repo" >> $corals_file
-                end
-            else
-                echo "$portable_repo" >> $corals_file
-            end
         else
             echo "Failed to clone $repo"
         end
@@ -78,35 +68,24 @@ function __reef_add
 end
 
 function __reef_rm
-    set -l corals_file $__fish_config_dir/fish_plugins
     for coral in $argv
         set -l path "$__fish_config_dir/corals/$coral"
         set -l name (string replace -r '^.*?([^/]*)$' '$1' $coral)
         if test -d "$path"
             command rm -rf "$path"
             echo "🪸🐟 removed coral: $coral"
+            emit reef_rm "$coral"
             emit {$name}_uninstall
-
-            if test -f $corals_file
-                set -l tmp (mktemp)
-                set -l coral_slug (__reef_resolve_coral $coral)
-                while read -l line
-                    set -l resolved (__reef_resolve_coral $line)
-                    if test "$resolved" != "$coral_slug"
-                        echo "$line" >> $tmp
-                    end
-                end < $corals_file
-                mv $tmp $corals_file
-            end
         else
             echo 🪸🐟 (set_color red)coral not found: $coral(set_color normal)
         end
     end
 end
 
-function __reef_up
+function __reef_update
     set -l corals $argv
     if not set -q argv[1]
+        functions -q __reef_on_update; and __reef_on_update
         set corals (__reef_list)
     end
 
@@ -115,39 +94,6 @@ function __reef_up
         printf "%-30s " $coral
         git -C "$__fish_config_dir/corals/$coral" pull && emit {$s_name}_update || echo "Failed to update $coral"
     end
-end
-
-function __reef_sync
-    set -l corals_file $__fish_config_dir/fish_plugins
-
-    if test -f $corals_file
-        set -l desired_lines (string trim < $corals_file | string match -r -v '^#|^$')
-        set -l installed (__reef_list)
-
-        set -l missing
-        set -l desired_corals
-        for line in $desired_lines
-            set -l resolved (__reef_resolve_coral $line)
-            set -a desired_corals $resolved
-            if not contains $resolved $installed
-                set -a missing $line
-            end
-        end
-
-        set -l extra
-        for coral in $installed
-            if not contains $coral $desired_corals
-                # Don't self-destruct reef if it's not in the file
-                test "$coral" = "danielb2/reef"; and continue
-                set -a extra $coral
-            end
-        end
-
-        test -n "$missing"; and __reef_add $missing
-        test -n "$extra"; and __reef_rm $extra
-    end
-
-    __reef_up
 end
 
 function reef -d 'package manager for fish'
@@ -170,11 +116,7 @@ function reef -d 'package manager for fish'
             __reef_rm $argv
             reef reload
         case up update upgrade
-            if not set -q argv[1]
-                __reef_sync
-            else
-                __reef_up $argv
-            end
+            __reef_update $argv
             reef reload
         case reload
             for i in $__fish_config_dir/corals/**/reef/*/reef.fish
