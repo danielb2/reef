be5653a67a4942cb7d81a05f4d2a0f730232e7b3
Daniel Bretoi <daniel@otherware.org>
2026-05-26 11:49:07 +0700

    update lb commands for completions

--- completions/reef.fish
+++ completions/reef.fish
@@ -1,59 +1,32 @@
-function __reef_list_corals
-    for coral in $__fish_config_dir/corals/*/*
-        echo (string replace -r "^$__fish_config_dir/corals/" "" $coral)
-    end
+function __fish_lb_tags
+    lb ls
 end
 
-set -l __reef_commands install add rm remove up update list theme
+complete -c lb -e
+complete -c lb -f
+complete -c lb -n "__fish_use_subcommand" -a "add" -d "Add an entry"
+complete -c lb -n "__fish_use_subcommand" -a "ls" -d "List tags or entries"
+complete -c lb -n "__fish_use_subcommand" -a "log" -d "List entries chronologically"
+complete -c lb -n "__fish_use_subcommand" -a "search" -d "Search entries"
+complete -c lb -n "__fish_use_subcommand" -a "rm" -d "Remove an entry or tag"
+complete -c lb -n "__fish_use_subcommand" -a "edit" -d "Edit an entry or tag's entries"
+complete -c lb -n "__fish_use_subcommand" -a "info" -d "Show tool info"
+complete -c lb -n "__fish_use_subcommand" -a "init" -d "Initialize configuration or completions"
+complete -c lb -n "__fish_use_subcommand" -a "push" -d "Push to Joplin"
+complete -c lb -n "__fish_use_subcommand" -a "bkup" -d "Backup database"
+complete -c lb -n "__fish_use_subcommand" -a "cal" -d "Show calendar"
 
-complete -e reef
+complete -c lb -n "__fish_seen_subcommand_from init" -a "fish" -d "Generate fish completions"
 
-complete -c reef -n "not __fish_seen_subcommand_from $__reef_commands" -f \
-    -a "add install" \
-    -d "add corals"
-
-complete -c reef -n "not __fish_seen_subcommand_from $__reef_commands" -f \
-    -a "rm remove" \
-    -d "remove corals"
-
-complete -c reef -n "not __fish_seen_subcommand_from $__reef_commands" -f \
-    -a "up update" \
-    -d "update coral(s)"
-
-complete -c reef -n "not __fish_seen_subcommand_from $__reef_commands" -f \
-    -a version \
-    -d "display reef version"
-
-complete -c reef -n "not __fish_seen_subcommand_from $__reef_commands" -f \
-    -a theme \
-    -d "list or set new theme"
-
-complete -c reef -n "not __fish_seen_subcommand_from $__reef_commands" -f \
-    -a splash \
-    -d "show the reef splash"
-
-complete -c reef -n "not __fish_seen_subcommand_from $__reef_commands" -f \
-    -a fish_reload \
-    -d "reload the fish shell"
-
-complete -c reef -n "not __fish_seen_subcommand_from $__reef_commands" -f \
-    -a reload \
-    -d "reload reefs files (you probably dont need this)"
-
-complete -c reef -n "not __fish_seen_subcommand_from $__reef_commands" -f \
-    -a help \
-    -d "this beautiful help"
-
-complete -c reef -n "__fish_seen_subcommand_from install" -a "" \
-    -d "Git URL or user/repo to install"
-
-complete -c reef -f -n "__fish_seen_subcommand_from rm remove up update" \
-    -a "(__reef_list_corals)" \
-    -d "installed coral"
-
-complete -c reef -f -n "__fish_seen_subcommand_from theme" \
-    -a "(reef_list_themes)" \
-    -d 'set theme'
-
-complete -c reef -f -d 'list installed corals' -a 'ls list' -n "not __fish_seen_subcommand_from $__reef_commands"
-complete -c reef -f -n '__fish_seen_subcommand_from ls list version'
+complete -c lb -n "__fish_seen_subcommand_from ls" -s l -d "Long format"
+complete -c lb -n "__fish_seen_subcommand_from ls" -s v -d "Verbose (-v, -vv)"
+complete -c lb -n "__fish_seen_subcommand_from ls" -s a -d "All entries (chronological)"
+complete -c lb -n "__fish_seen_subcommand_from log" -s l -d "Long format"
+complete -c lb -n "__fish_seen_subcommand_from log" -s v -d "Verbose (-v, -vv)"
+complete -c lb -n "__fish_seen_subcommand_from search" -s l -d "Long format"
+complete -c lb -n "__fish_seen_subcommand_from search" -s v -d "Verbose (-v, -vv)"
+complete -c lb -n "__fish_seen_subcommand_from edit" -s y -d "YAML mode"
+complete -c lb -n "__fish_seen_subcommand_from cal" -s y -d "Year view"
+complete -c lb -n "__fish_seen_subcommand_from cal" -s i -d "Include tags"
+complete -c lb -n "__fish_seen_subcommand_from cal" -s x -d "Exclude tags"
+complete -c lb -n "__fish_seen_subcommand_from add ls rm mv edit cal" -a "(__fish_lb_tags)"
