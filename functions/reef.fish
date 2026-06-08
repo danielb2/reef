2f99daa203c2ad43bdf489a9e04b9772ba7de0a5
Daniel Bretoi <daniel@otherware.org>
2026-06-08 09:37:37 +0700

    fix: call reef reload in reef init to ensure functions are available

--- functions/reef.fish
+++ functions/reef.fish
@@ -132,6 +132,7 @@ function reef -d 'package manager for fish'
             mkdir -p "$__fish_config_dir/conf.d"
             set -l reef_path "$__fish_config_dir/corals/danielb2/reef"
             echo "source $reef_path/conf.d/reef.fish" >"$__fish_config_dir/conf.d/reef.fish"
+            reef reload
             echo 🪸🐟 initialized - ready to go
             reef splash
         case splash
